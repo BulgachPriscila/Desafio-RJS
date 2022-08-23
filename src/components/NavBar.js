01e854f2913854d3f185ee4dc3936683508dd833
Priscila Bulgach <bulgachpriscila@gmail.com>
2022-08-22 21:20:13 -0300

    Agregando Nav

---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import './NavBar.css'
+
+const NavBar = () => {
+    return (
+    <div>
+        <nav className='nav-menu'>
+            <a>Bienvenidos</a>
+            <a>Inicio</a>
+            <a>Quiénes somos</a>
+            <a>Contacto</a>
+        </nav>
+    </div>
+    )
+}
+
+export default NavBar
